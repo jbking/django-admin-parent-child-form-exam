e69c7f9c288a3f0fbccd4b12c6c3005b227fe942
Yusuke MURAOKA <yusuke@jbking.org>
2020-10-02 23:09:34 +0900

    pass item's id to add_view by query string

--- myproj/myapp/admin.py
+++ myproj/myapp/admin.py
@@ -1,10 +1,24 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
+from django.utils.http import urlencode
 
 from myapp.models import Item, ItemHistory
 
 
 class ItemAdmin(admin.ModelAdmin):
-    pass
+    list_display = [
+        'name',
+        'add_history',
+    ]
+
+    def add_history(self, item):
+        return format_html(
+            '<a href="{}?{}">click</a>',
+            reverse('admin:myapp_itemhistory_add'),
+            # give the id with foreign key name `item`
+            urlencode({'item': item.id}),
+        )
 
 
 class ItemHistoryAdmin(admin.ModelAdmin):
